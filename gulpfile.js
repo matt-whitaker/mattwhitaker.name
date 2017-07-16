894da5dfbbb7d3c06e67dd43d949fa979c4e0fa3
Matt Whitaker <matt.whitaker@concur.com>
2017-07-15 22:02:20 -0700

    moar

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const inject = require('gulp-inject');
-const htmlmin = require('gulp-htmlmin');
-const browserSync = require('browser-sync').create();
+const gulp          = require('gulp');
+const less          = require('gulp-less');
+const inject        = require('gulp-inject');
+const htmlmin       = require('gulp-htmlmin');
+const mustache      = require('gulp-mustache');
+const rename        = require('gulp-rename');
+const browserSync   = require('browser-sync').create();
+const pkg           = require('./package.json');
 
 const port = process.env.PORT || 8080;
 const serveRoot = 'lib';
 const srcRoot = 'src';
 
 // Static server
-gulp.task('serve', ['index', 'less'], function() {
+gulp.task('serve', ['html'], function() {
   browserSync.init({
     port,
     injectChanges: true,
@@ -21,24 +24,25 @@ gulp.task('serve', ['index', 'less'], function() {
 
   gulp.watch([
     `${srcRoot}/**/*.less`,
-    `${srcRoot}/**/*.html`
-  ], ['index']);
+    `${srcRoot}/**/*.mustache`
+  ], ['html']);
 });
 
 // Compile less into CSS & auto-inject into browsers
-gulp.task('less', () => {
+gulp.task('css', () => {
   return gulp.src(`${srcRoot}/less/**/*.less`)
     .pipe(less())
     .pipe(gulp.dest(`${serveRoot}/css`));
 });
 
-gulp.task('index', ['less'], () => {
+gulp.task('html', ['css'], () => {
+  const sources = gulp.src(`./${serveRoot}/**/*.css`, {read: false});
 
-  const sources = gulp.src([`./${serveRoot}/**/*.css`], {read: false});
-
-  return gulp.src(`${srcRoot}/index.html`)
+  return gulp.src(`${srcRoot}/**/*.mustache`)
     .pipe(inject(sources, { ignorePath: serveRoot }))
+    .pipe(mustache(pkg[pkg.name]))
     .pipe(htmlmin({ removeComments: true }))
+    .pipe(rename({ extname: '.html' }))
     .pipe(gulp.dest(serveRoot))
     .pipe(browserSync.stream());
 });
