305e4e77fe3a57b3478da41637730ceff72f8028
Matt Whitaker <matt.whitaker@concur.com>
2017-12-17 23:00:11 -0800

    change date bar implementation to responsive

--- src/components/shell.jsx
+++ src/components/shell.jsx
@@ -3,10 +3,19 @@ import Markdown from 'markdown-to-jsx';
 import classnames from 'classnames';
 import Nav from './nav';
 
+const renderDate = (blog) => blog ? (
+    <time className="mw-blog-date">
+      <span className="mw-date-value">{blog.date}</span>
+      <span className="mw-date-bar" />
+    </time>
+  ): null;
+
 const renderPage = (props) => {
   const { Page } = props;
   return typeof Page === 'string' ? <Markdown>{Page}</Markdown> : <Page {...props} />;
 };
+
+const renderPageNav = (blog) => blog ? 'prev | next' : null;
 
 export default (props) => [
   <header className="mw-header">
@@ -16,12 +25,12 @@ export default (props) => [
   </header>,
   <main className={classnames({ 'mw-main': true, 'mw-blog': props.$page.blog })}>
     <article>
-      { props.$page.blog ? <time className="mw-blog-date">{props.$page.blog.date}</time> : null }
+      {renderDate(props.$page.blog)}
       <h1 className="mw-page-title">{props.$page.title}</h1>
       {renderPage(props)}
     </article>
   </main>,
   <footer className="mw-footer" style={{textAlign: 'center'}}>
-    { props.$page.blog ? 'prev | next' : null }
+    {renderPageNav(props.$page.blog)}
   </footer>
 ];
