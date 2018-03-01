f91e4a376a94530215d3e2da36e5f163208e30de
Matt Whitaker <matt.whitaker@concur.com>
2018-02-28 22:48:21 -0800

    wip

--- bin/static-deploy.sh
+++ bin/static-deploy.sh
@@ -8,10 +8,12 @@ if [ "${CIRCLE_BRANCH}" == "master" ]; then
 
   echo "Deployed to static site from branch master"
 elif [ "${CIRCLE_BRANCH}" == "develop" ]; then
-  NODE_ENV=qa \
-  AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID_QA \
-  AWS_S3_BUCKET=$AWS_S3_BUCKET_QA \
-  npm run deploy
+  # NODE_ENV=qa \
+  # AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID_QA \
+  # AWS_S3_BUCKET=$AWS_S3_BUCKET_QA \
+  # npm run deploy
 
-  echo "Deployed to static site from branch develop"
+
+  echo "deploy disabled"
+  # echo "Deployed to static site from branch develop"
 fi
