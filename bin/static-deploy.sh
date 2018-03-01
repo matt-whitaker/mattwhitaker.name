6dc414e858aed098bf7cbe6f953b749b2f19d1ac
Matt Whitaker <matt.whitaker@concur.com>
2018-03-01 00:18:18 -0800

    some cleanup

--- bin/static-deploy.sh
+++ bin/static-deploy.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
-  NODE_ENV=production \
-  AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID_PROD \
-  AWS_S3_BUCKET=$AWS_S3_BUCKET_PROD \
-  npm run build
-
-  echo "Deployed to static site from branch master"
+  AWS_S3_BUCKET=$AWS_S3_BUCKET_PROD
+  AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID_PROD
 elif [ "${CIRCLE_BRANCH}" == "develop" ]; then
-  echo "Syncing with S3"
-
-  aws s3 sync ./lib s3://$AWS_S3_BUCKET_QA/ \
-    --delete \
-    --acl public-read \
-
-  echo "Synced with S3"
-
-  aws configure set preview.cloudfront true
-
-  echo "Invalidating Cloudfront"
-
-  aws cloudfront create-invalidation \
-    --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID_QA \
-    --paths "/"
-
-  echo "Invalidated Cloudfront"
-
-  echo "Deployed to static site from branch develop"
+  AWS_S3_BUCKET=$AWS_S3_BUCKET_QA
+  AWS_CLOUDFRONT_DISTRIBUTION_ID=$AWS_CLOUDFRONT_DISTRIBUTION_ID_QA
 fi
+
+echo "Syncing with S3"
+
+aws s3 sync ./lib s3://$AWS_S3_BUCKET/ \
+--delete \
+--acl public-read \
+
+echo "Synced with S3"
+
+aws configure set preview.cloudfront true
+
+echo "Invalidating Cloudfront"
+
+aws cloudfront create-invalidation \
+--distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID \
+--paths "/" \
+>/dev/null
+
+echo "Invalidated Cloudfront"
+
+echo "Deployed to static site from branch $CIRCLE_BRANCH"
