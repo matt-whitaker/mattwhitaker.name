beb34ddff60bff873b7400508c29269d4e9adcc4
Matt Whitaker <mattwhitaker.name@gmail.com>
2024-07-09 17:46:00 -0700

    feat: lang for www

--- packages/www/tailwind.config.js
+++ packages/www/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./template/**/*.ejs"],
+  content: ["./template/**/*.ejs", "./lang.txt"],
   corePlugins: {
     preflight: false,
   },
