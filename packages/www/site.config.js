e21a6c0f02e2e796179dcbb6c2f8f1ab4605d52c
Matt Whitaker <mattwhitaker.name@gmail.com>
2026-07-04 01:02:48 -0700

    touchup

--- packages/www/site.config.js
+++ packages/www/site.config.js
@@ -2,6 +2,7 @@ export default {
   email: 'contact@mattwhitaker.name',
   linkedin: 'https://www.linkedin.com/in/whitakermatt',
   github: 'https://github.com/matt-whitaker',
+  emwhit: "https://emwhitproject.com",
   fonts: {
     urbanist: 'https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap'
   },
